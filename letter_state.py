1d712b2e59d42b3138518a5ed4ed12f61d088074
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-05-05 18:56:36 +0100

    Refactor wordle classes to be include on a new package

--- letter_state.py
+++
@@ -1,9 +0,0 @@
-class LetterState:
-    def __init__(self, character: str):
-        self.character = character
-
-        self.is_in_word : bool = False
-        self.is_in_position : bool = False
-
-    def __repr__(self)-> str:
-        return f"{self.character} is in word: {self.is_in_word}, is in position: {self.is_in_position}"
