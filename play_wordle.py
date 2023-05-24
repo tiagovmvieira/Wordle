aaf8054e47c7f8e5185c264ac880131a536f9d6c
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-05-24 18:30:58 +0100

    Refactor play_wordle interface to extend parser logic and a new report

--- play_wordle.py
+++ play_wordle.py
@@ -1,6 +1,8 @@
 import sys
 import argparse
 
+from dotenv import load_dotenv
+
 from wordle.wordle import Wordle
 from wordle.reporter import Reporter
 
@@ -10,13 +12,20 @@ def main():
         parser.add_argument("--word_lenght", type=int,
                             help="The length of the word to be guessed (default: %(default)s)",
                             choices=range(5, 11), default=5, metavar='[5-10]')
+        parser.add_argument("--game_mode", type=str,
+                            help="The game mode to play",
+                            choices=["single_player", "multi_player"],
+                            required=True)
         return parser.parse_args()
 
     args = parse_arguments()
+    load_dotenv()
 
     print("Hello Wordle!")
-    wordle = Wordle(word_length=args.word_lenght)
+    wordle = Wordle(word_length=args.word_lenght, game_mode=args.game_mode)
     reporter = Reporter(wordle=wordle)
+    
+    reporter.report(game_configs=True)
     reporter.report(initial_message=True)
 
     while wordle.can_attempt:
